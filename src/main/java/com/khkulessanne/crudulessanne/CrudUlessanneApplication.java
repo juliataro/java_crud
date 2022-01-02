07d5794fdfebb8918cc66974c8b3ca901e7c3b4c
juliataro <julia.taro@gmail.com>
2022-01-02 19:39:52 +0200

    Initial commit

---
+++ src/main/java/com/khkulessanne/crudulessanne/CrudUlessanneApplication.java
@@ -0,0 +1,13 @@
+package com.khkulessanne.crudulessanne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUlessanneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUlessanneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khkulessanne/crudulessanne/CrudUlessanneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khkulessanne.crudulessanne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUlessanneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
